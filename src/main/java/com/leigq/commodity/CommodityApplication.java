68458b83d99cb74970dbc4f9324dd88d3df712ae
leigq <191789784@qq.com>
2019-03-07 15:37:00 +0800

    添加 Feign 依赖

--- src/main/java/com/leigq/commodity/CommodityApplication.java
+++ src/main/java/com/leigq/commodity/CommodityApplication.java
@@ -3,9 +3,11 @@ package com.leigq.commodity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class CommodityApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/leigq/commodity/controller/ServerController.java
+++ src/main/java/com/leigq/commodity/controller/ServerController.java
@@ -1,13 +1,10 @@
 package com.leigq.commodity.controller;
 
-import com.netflix.discovery.converters.Auto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
- * restTemplate 远程调用服务端简单测试
+ * restTemplate、Feign 远程调用服务端简单测试
  * <p>
  * 创建人：LeiGQ <br>
  * 创建时间：2019-03-07 13:47 <br>
@@ -22,7 +19,7 @@ public class ServerController {
 
     @GetMapping("/messages")
     public String getMsg() {
-        return "调用商品服务2成功。。。";
+        return "调用商品服务1成功。。。";
     }
 
 }
