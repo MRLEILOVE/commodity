9f4abb458f72c9383044d6abd0e0c80f233d4b98
leigq <191789784@qq.com>
2019-03-07 14:52:52 +0800

    添加 restTemplate 远程调用测试接口

---
+++ src/main/java/com/leigq/commodity/controller/ServerController.java
@@ -0,0 +1,28 @@
+package com.leigq.commodity.controller;
+
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * restTemplate 远程调用服务端简单测试
+ * <p>
+ * 创建人：LeiGQ <br>
+ * 创建时间：2019-03-07 13:47 <br>
+ * <p>
+ * 修改人： <br>
+ * 修改时间： <br>
+ * 修改备注： <br>
+ * </p>
+ */
+@RestController
+public class ServerController {
+
+    @GetMapping("/messages")
+    public String getMsg() {
+        return "调用商品服务2成功。。。";
+    }
+
+}
